2dbd2e4ed9586aa178eb0b6f3336a00b2850b0ab
victor braulio zevallos castillo <zevallos.v88@gmail.com>
2025-09-19 17:30:50 -0500

    Add files via upload

---
+++ Program sumatoria.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace sumatoria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sumatoria de números: ");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Ingrese un número: ");
+            string numero = Console.ReadLine();
+            // ahora trabajemos con enteros
+            int.TryParse(numero, out int numeroEntero);
+            int suma = 0;
+            Console.WriteLine("-------------------------: ");
+            Console.WriteLine("La respuesta es: ");
+            for (int i = 1; i <= numeroEntero; i++)
+            {
+                suma += i; // suma = suma + i
+                
+                Console.WriteLine($"La sumatoria hasta {i} es: {suma} ");
+               
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ calificaion listo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calificaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calificacion de nota:");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("ingrese su nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("ingrese una calificacion");
+            Console.WriteLine("--------------------------");
+            string nota = Console.ReadLine();
+            // ahora trabajamos con decimales
+            double.TryParse(nota, out double calificacionDecimal);
+            // Regla: si parte decimal < 0.5 queda igual, si es >= 0.5 aumenta en 1
+            int calificacion = (int)calificacionDecimal;
+            // parte entera
+            double decimales = calificacionDecimal - calificacion;
+            if (decimales >= 0.5)
+            {
+                calificacion++;
+            }
+
+
+            //validar rango
+            // Validamos rango
+            if (calificacion < 0 || calificacion > 20)
+            {
+                Console.WriteLine($"{nombre}, su calificación {calificacion} es inválida.");
+            }
+            else if (calificacion <= 7)
+            {
+                Console.WriteLine($"{nombre}, su calificación {calificacion} es deficiente.");
+            }
+            else if (calificacion <= 10)
+            {
+                Console.WriteLine($"{nombre}, su calificación {calificacion} es mejorable.");
+            }
+            else if (calificacion <= 13)
+            {
+                Console.WriteLine($"{nombre}, su calificación {calificacion} es regular.");
+            }
+            else if (calificacion <= 16)
+            {
+                Console.WriteLine($"{nombre}, su calificación {calificacion} es buena.");
+            }
+            else // 17 - 20
+            {
+                Console.WriteLine($"{nombre}, su calificación {calificacion} es excelente.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+
+   
