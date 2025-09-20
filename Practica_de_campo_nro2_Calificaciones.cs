aa02f65d4f2fc68736e148fac56dc11f3ede6efa
victor braulio zevallos castillo <zevallos.v88@gmail.com>
2025-09-19 20:39:55 -0500

    Practica_de_campo_nro2_Calificaciones.cs

--- Practica_de_campo_nro2_Calificaciones.cs
+++ Practica_de_campo_nro2_Calificaciones.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace calificaciones
+namespace Calificaciones
 {
     internal class Program
     {
@@ -87,5 +87,6 @@ namespace calificaciones
 
 
           
+
 
    
